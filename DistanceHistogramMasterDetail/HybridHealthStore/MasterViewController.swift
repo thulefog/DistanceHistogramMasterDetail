e4da2ca21970705c5d21c031eb9ff926f24e4c3b
John Weston <thulefog@yahoo.com>
2016-01-23 21:49:04 -0800

    shifted to pod file approach based on travis iterations

--- DistanceHistogramMasterDetail/HybridHealthStore/MasterViewController.swift
+++ DistanceHistogramMasterDetail/HybridHealthStore/MasterViewController.swift
@@ -267,7 +267,7 @@ class MasterViewController: UITableViewController {
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
-    }
+    } 
 
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
